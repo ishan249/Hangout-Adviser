1cb630af75f8274c44556f320c639c5e98e3b5f7
ishan249 <249patelishan@gmail.com>
2023-01-01 20:22:45 +0530

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Headers", "*");
+    res.set("Access-Control-Allow-Methods", "*");
+    res.set("x-requested-with", "XMLHttpRequest");
+    res.set("Access-Control-Expose-Headers","Content-Encoding,api_key");
+    res.set("origin","http://localhost:3000");
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        return;
+    }
+    next();
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    axios.get('https://serpapi.com/search.json?engine=google&q=Coffee&location=Austin%2C+Texas%2C+United+States&google_domain=google.com&gl=us&hl=en&api_key=891d25145df260a9fe20a0c64000f46fa08472219b27d0fbf3da0b7f96768b7a')
+        .then(function (response) {
+            res.send(response.data);
+        })
+        .catch(function (error) {
+            console.log("reaching here and printing your error")
+
+            console.log(error.response);
+        });
+});
+
+app.listen(5000, ()=>{
+    console.log("app is listening on port 5000");
+});
